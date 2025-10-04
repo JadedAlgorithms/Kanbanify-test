4217a4005702a68e315d31fb9e454e78c40139d6
JadedAlgorithms <algorfidelis247@gmail.com>
2025-10-04 17:04:08 +0530

    fix #11 Improved error handling and validation in card and comment controllers

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -13,24 +13,27 @@ class CardController {
   };
 
   createCard = async (req, res) => {
+    try{
     const { title, description, typeId } = req.body;
     const projectId = req.params.projectId;
     const card = await addCardToDB(title, description, typeId, projectId);
     res.status(200).json({ message: "Card created successfully", card });
-  };
-
+  } catch(error) {res.status(500).json({error: `Error creating card: ${error.message}`})};
   deleteCard = async (req, res) => {
+    try{
     const cardId = req.params.cardId;
     const card = await deleteCardFromDB(cardId);
     res.status(200).json({ message: "Card deleted successfully", card });
-  };
+    }catch(error) {res.status(500).json({error: `Error deleting card: ${error.message}`})};
 
   updateCard = async (req, res) => {
+    try{
     const cardId = req.params.cardId;
     const { title, description, typeId } = req.body;
     const card = await updateCardFromDB(cardId, title, description, typeId);
     res.status(200).json({ message: "Card updated successfully", card });
-  };
+  } catch(error) {res.status(500).json({error: `Error updating card: ${error.message}`})};
+  }
 }
 
 module.exports = new CardController();
